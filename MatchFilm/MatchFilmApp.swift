41b20fe0985bf55f5e0e953115c864dffc17d702
Pasha Hurs <hurs_pasha@mail.ru>
2025-04-19 18:13:12 +0300

    Initial Commit

---
+++ MatchFilm/MatchFilmApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchFilmApp.swift
+//  MatchFilm
+//
+//  Created by Pasha Hurs on 19.04.25.
+//
+
+import SwiftUI
+
+@main
+struct MatchFilmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MatchFilmTests/MatchFilmTests.swift
@@ -0,0 +1,17 @@
+//
+//  MatchFilmTests.swift
+//  MatchFilmTests
+//
+//  Created by Pasha Hurs on 19.04.25.
+//
+
+import Testing
+@testable import MatchFilm
+
+struct MatchFilmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
